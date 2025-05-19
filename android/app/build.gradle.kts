09b190d46fe5f17a7d4370c213ba49bdce1f4f0c
RoadsidePros <RoadsidePros247@gmail.com>
2025-05-19 18:36:46 -0400

    Build #98

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.roadsidepros.roadsidepros"
-        minSdk = 19
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
